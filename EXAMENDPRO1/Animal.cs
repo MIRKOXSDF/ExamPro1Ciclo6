cd92bf0daf1f738d9e1aa082cf2f2a34c492d902
MIRKOXSDF <mirko06660o@gmail.com>
2024-09-21 18:31:13 -0500

    Crear clase , interface Animal

---
+++ EXAMENDPRO1/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAMENDPRO1
+{
+    class Animal : IAnimal
+    {
+        public string nombre;
+        public int cantidadDeLeche;
+
+        public Animal(string name, int cantidadDeLeche)
+        {
+            this.nombre = name;
+            this.cantidadDeLeche = cantidadDeLeche;
+        }
+
+        public void Ordeñar()
+        {
+            if (cantidadDeLeche > 0)
+            {
+                Console.WriteLine($"Ordeñaste a {nombre} y obtuviste {cantidadDeLeche} litros de leche.");
+
+            }
+            else
+            {
+                Console.WriteLine($"{nombre} no tiene leche disponible.");
+
+            }
+        }
+    }
+}
